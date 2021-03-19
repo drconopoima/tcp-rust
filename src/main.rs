a74bbef31504e5d9f0f47dfa7bb2fbf263e549e1
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-03-19 13:18:30 +0100

    Initial single ip packet tun_tap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use tun_tap;
+
+fn main() -> io::Result<()> {
+    let nic = tun_tap::Iface::new("tun%d", tun_tap::Mode::Tun)?;
+    let mut buf = [0u8; 1504];
+    let nbytes = nic.recv(&mut buf[..])?;
+    eprintln!("read {} bytes: {:02x?}", nbytes, &buf[..nbytes]);
+    Ok(())
 }
