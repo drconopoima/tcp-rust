6e092251fcdd793a61cb8c91cf57503d38c36815
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2021-03-20 12:21:44 +0100

    Add script for initialization of tun0 ipv4

---
+++ run_tcp-rust.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -eEuo pipefail
+script_file=$(find "${0}" -type f)
+readonly script_file
+script_name=$(basename --suffix ".sh" -- "${script_file}")
+readonly script_name
+script_version='0.1.0'
+readonly script_version
+
+printf "%s (%s)\n" "${script_name}" "${script_version}"
+lockfile="/var/lock/${script_name}.lock"
+readonly lockfile
+
+function handler_cleanup() {
+    if grep -q -- $$ "${lockfile}"; then
+        rm -v -- "${lockfile}" && echo "Removed lock ${lockfile}"
+    else
+        printf "Lock %s is currently held by process %s. Not removing.\n" "${lockfile}" "$(cat "${lockfile}")"
+    fi
+}
+readonly -f handler_cleanup
+
+trap handler_cleanup EXIT
+
+readonly CARGO_TARGET_DIR=${1:-"./target"}
+readonly IP_RANGE_TUNTAP=${2:-"192.168.0.1/24"}
+readonly TUNTAP_DEVICE_NAME=${3:-"tun0"}
+readonly binary="tcp-rust"
+
+if [[ ! -f "${lockfile}" ]]; then
+    (echo $$ >"${lockfile}") && echo "Adquired lock ${lockfile}"
+    set -x
+    cargo build --release
+    sudo -p "Please enter your password: " whoami 1>/dev/null && {
+        sudo setcap cap_net_admin=eip "$CARGO_TARGET_DIR/release/${binary}"
+    }
+    command=("$CARGO_TARGET_DIR/release/${binary}")
+    ${command[0]} &
+    readonly PROCESS_PID="$!"
+    sudo -p "Please enter your password: " whoami 1>/dev/null && {
+        sudo ip addr add "${IP_RANGE_TUNTAP}" dev "${TUNTAP_DEVICE_NAME}"
+        sudo ip link set up dev "${TUNTAP_DEVICE_NAME}"
+    }
+    function handler_stop() {
+        set +eEuo pipefail
+        kill -- "$PROCESS_PID"
+        (ps -o comm -p "${PROCESS_PID}" | head -n+2 | grep -q -- "${binary}") && kill -9 -- "${PROCESS_PID}"
+    }
+    readonly -f handler_stop
+    trap handler_stop SIGHUP SIGINT SIGQUIT SIGTERM # 1 2 3 15
+
+    wait -- "${PROCESS_PID}"
+fi
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ use tun_tap;
 fn main() -> io::Result<()> {
     let nic = tun_tap::Iface::new("tun%d", tun_tap::Mode::Tun)?;
     let mut buf = [0u8; 1504];
-    let nbytes = nic.recv(&mut buf[..])?;
-    eprintln!("read {} bytes: {:02x?}", nbytes, &buf[..nbytes]);
-    Ok(())
+    loop {
+        let nbytes = nic.recv(&mut buf[..])?;
+        eprintln!("read {} bytes: {:02x?}", nbytes, &buf[..nbytes]);
+    }
 }
